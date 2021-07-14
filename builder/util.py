f769bbe4844e269304fe2e707c443b47319fac3a
kavink <kavink@nvidia.com>
2021-07-14 12:55:46 -0700

    Add preliminary builder code

---
+++ builder/util.py
@@ -0,0 +1,10 @@
+import docker
+import os
+import shutil
+import pathlib
+import logging
+
+logger = logging.getLogger(__name__)
+
+def package_application(package_name: str, base_image: str, entrypoint: str, app_config: str, package_config: str):
+    pass
